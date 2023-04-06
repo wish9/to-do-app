c7dcf237a5ef3b234fa5b6309b05b7d72bdb8f5b
wish9 <wjwee9@gmail.com>
2023-04-07 07:49:44 +0900

    JAR → WAR 포맷 변경

--- src/main/java/com/wish/todoapp/ToDoAppApplication.java
+++ src/main/java/com/wish/todoapp/ToDoAppApplication.java
@@ -2,12 +2,20 @@ package com.wish.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { // JAR → WAR 포맷 변경
+	    return builder.sources(ToDoAppApplication.class);
 	}
 
 }
